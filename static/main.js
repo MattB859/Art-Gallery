dee7829a4e86b639a3ffad64c44e276ffcce4405
MattB859 <mateus756@hotmail.com>
2022-02-23 17:01:18 +0000

    Added navbar menu to display nav links when clicked

--- static/main.js
+++ static/main.js
@@ -1,3 +1,12 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+  function openNav() {
+    
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
