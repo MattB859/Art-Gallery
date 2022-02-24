3fe2772acccf05635fee1253fb521cd9f2253aab
MattB859 <mateus756@hotmail.com>
2022-02-24 17:01:03 +0000

    Add function to display art image when mouse moves over artists names

--- static/main.js
+++ static/main.js
@@ -1,21 +1,29 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
+$('.main-carousel').flickity({
   cellAlign: 'left',
   contain: true
 });
 
-// element argument can be a selector string
-//   for an individual element
 var flkty = new Flickity( '.main-carousel', {
-  // options
+
 });
 
-// Toggle menu
+// Function to toggle the navigation menu
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
 }
 
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
+}
+
+
+// Custom function to show art work when mouse moves over artist names
+function showArt(){
+  const show = document.querySelector('.show-art');
+  show.style.display = "block"
+}
+
+function removeArt(){
+  const show = document.querySelector('.show-art');
+  show.style.display = "none"
 }
